b850eaf36643cfcc717612a4379214eab22a59d9
Michael Dziedzic <michaeldzie@gmail.com>
2016-09-07 19:34:00 -0700

    (refactor)

--- processHTMLHelper.js
+++ processHTMLHelper.js
@@ -3,11 +3,11 @@ const glob = require('glob');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = () => {
-  const realContentFolderPath = fs.realpathSync(`${__dirname}/source/`);
+  const realContentFolderPath = `${fs.realpathSync(`${__dirname}/source/`)}/`;
   const pages = glob.sync(`${__dirname}/source/**/*.html`);
 
   return pages.map(page => {
-    const filename = page.replace(realContentFolderPath, '').slice(1);
+    const filename = page.replace(realContentFolderPath, '');
     return new HtmlWebpackPlugin({
       template: page,
       filename,
