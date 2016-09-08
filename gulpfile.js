b695abb6439338ccda8b4474fdf1b6b85c1e51b4
Michael Dziedzic <michaeldzie@gmail.com>
2016-09-08 16:09:19 -0700

    (refactor)

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,6 @@ const argv = require('yargs').argv;
 
 gulp.task('images', () => {
   gulp.src(['source/images/**/*.{jpg,jpeg,png,gif}'], { base: 'source/images' })
-    .pipe(ifElse(argv.compress, () => {
-      return imagemin({
-        progressive: true,
-      });
-    }))
+    .pipe(ifElse(argv.compress, () => imagemin()))
     .pipe(gulp.dest('build/images'));
 });
