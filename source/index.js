c7f522a3893dffe121efe0c09d63825605440e32
Michael Dziedzic <michaeldzie@gmail.com>
2016-09-07 19:57:36 -0700

    (task) comment out eslint no-console on index.js sample

--- source/index.js
+++ source/index.js
@@ -1,7 +1,7 @@
 import './style/style.scss';
 
 const saySomething = (something) => {
-  console.log(something);
+  console.log(something); // eslint-disable-line no-console
 };
 
 saySomething('Excepteur sint occaecat!');
