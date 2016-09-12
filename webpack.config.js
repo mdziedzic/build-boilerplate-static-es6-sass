213488724106e726fbcdc1b071a3beddf5a22433
Michael Dziedzic <michaeldzie@gmail.com>
2016-09-12 09:47:41 -0700

    (task) cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -25,10 +25,6 @@ module.exports = {
         loader: extractCSS.extract('style-loader', 'css?-minimize!postcss!sass'),
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-      },
-      {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader',
         query: {
